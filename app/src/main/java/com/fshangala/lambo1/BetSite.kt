b41f8fd225c56bfbcd51ad4606e0cf7e6ca80a32
fshangala <fshangala@gmail.com>
2023-02-12 20:59:52 +0200

    updated laser247 to work with the new mechanism yet to be tested.

--- app/src/main/java/com/fshangala/lambo1/BetSite.kt
+++ app/src/main/java/com/fshangala/lambo1/BetSite.kt
@@ -39,16 +39,10 @@ class BetSite(val name: String = "laser247.com") {
                             "inputOdds.dispatchEvent(ev1);\n" +
                             "inputOdds.dispatchEvent(ev2);"
                 }
-                return "var input = document.querySelectorAll(\"app-bet-slip input\")[2];\n" +
-                        "var inputOdds = document.querySelectorAll(\"app-bet-slip input\")[0];\n" +
-
-                        "var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, \"value\").set;\n" +
-                        "var ev1 = new Event('input', { bubbles: true});\n" +
-                        "var ev2 = new Event('change', { bubbles: true});\n" +
-
-                        "nativeInputValueSetter.call(input, '$stake');\n" +
-                        "input.dispatchEvent(ev1);\n" +
-                        "input.dispatchEvent(ev2);" +
+                return "var inputElements = document.querySelectorAll(\"app-bet-slip input\")\n" +
+                        "inputElements[2].value = 200\n" +
+                        "inputElements[2].dispatchEvent(new Event('input', { bubbles: true}));\n" +
+                        "inputElements[2].dispatchEvent(new Event('change', { bubbles: true}));" +
                         setodds
             }
             "lotus365.com" -> {
@@ -70,16 +64,10 @@ class BetSite(val name: String = "laser247.com") {
                             "inputOdds.dispatchEvent(ev1);\n" +
                             "inputOdds.dispatchEvent(ev2);"
                 }
-                return "var input = document.querySelectorAll(\"app-bet-slip input\")[2];\n" +
-                        "var inputOdds = document.querySelectorAll(\"app-bet-slip input\")[0];\n" +
-
-                        "var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, \"value\").set;\n" +
-                        "var ev1 = new Event('input', { bubbles: true});\n" +
-                        "var ev2 = new Event('change', { bubbles: true});\n" +
-
-                        "nativeInputValueSetter.call(input, '$stake');\n" +
-                        "input.dispatchEvent(ev1);\n" +
-                        "input.dispatchEvent(ev2);" +
+                return "var inputElements = document.querySelectorAll(\"app-bet-slip input\")\n" +
+                        "inputElements[2].value = 200\n" +
+                        "inputElements[2].dispatchEvent(new Event('input', { bubbles: true}));\n" +
+                        "inputElements[2].dispatchEvent(new Event('change', { bubbles: true}));" +
                         setodds
             }
         }
@@ -100,25 +88,33 @@ class BetSite(val name: String = "laser247.com") {
     fun eventListenerScript(): String {
         when(name){
             "laser247.com" -> {
-                return "function checkElements(){\n" +
-                        "    setTimeout(()=>{\n" +
-                        "        targetElements = document.querySelectorAll(\".odds_body button\")\n" +
-                        "        if (targetElements.length > 0){\n" +
-                        "            targetElements.forEach((item)=>{\n" +
-                        "                item.addEventListener(\"click\",(event)=>{\n" +
-                        "                var itemb = event.target\n" +
-                        "                targetElements.forEach((itema,index,arr)=>{\n" +
-                        "                    if(itema==itemb){\n" +
-                        "                        window.lambo.performClick(index,itemb.innerText)\n" +
-                        "                    }\n" +
-                        "                })\n" +
-                        "            })})\n" +
-                        "        } else {\n" +
-                        "            checkElements()\n" +
-                        "        }\n" +
-                        "    },1000)\n" +
+                return "var targetElements = document.querySelectorAll(\".odds_body button\");\n" +
+                        "function clickEventListener(event){\n" +
+                        "    checkElements();\n" +
+                        "    var a = event.target;\n" +
+                        "    var selected = null;\n" +
+                        "    if(a.className === \"odd-button__price\" || a.className === \"odd-button__volume\"){\n" +
+                        "        selected = a.parentElement.parentElement;\n" +
+                        "    } else {\n" +
+                        "        selected = event.target;\n" +
+                        "    }\n" +
+                        "    //console.log(selected.getAttribute(\"lambolistenning\"));\n" +
+                        "    window.lambo.buttonCount(targetElements.length);\n" +
+                        "    window.lambo.performClick(selected.getAttribute(\"lambolistenning\"));\n" +
                         "}\n" +
-                        "checkElements()"
+                        "function checkElements(){\n" +
+                        "    targetElements = document.querySelectorAll(\".odds_body button\")\n" +
+                        "    targetElements.forEach((item,index,arr)=>{\n" +
+                        "        if(item.hasAttribute(\"lambolistenning\")){\n" +
+                        "            //console.log(item.innerText);\n" +
+                        "        } else {\n" +
+                        "            item.addEventListener(\"click\",clickEventListener);\n" +
+                        "        }\n" +
+                        "        item.setAttribute(\"lambolistenning\",index);\n" +
+                        "    });\n" +
+                        "    window.lambo.buttonCount(targetElements.length);\n" +
+                        "}\n" +
+                        "checkElements();"
             }
             "lotus365.com" -> {
                 return "var targetElements = document.querySelectorAll(\".SportEvent__market .odd-button\");\n" +
@@ -151,97 +147,33 @@ class BetSite(val name: String = "laser247.com") {
                         "checkElements();"
             }
             else -> {
-                return "function checkElements(){\n" +
-                        "    setTimeout(()=>{\n" +
-                        "        targetElements = document.querySelectorAll(\".odds_body button\")\n" +
-                        "        if (targetElements.length > 0){\n" +
-                        "            targetElements.forEach((item)=>{\n" +
-                        "                item.addEventListener(\"click\",(event)=>{\n" +
-                        "                var itemb = event.target\n" +
-                        "                targetElements.forEach((itema,index,arr)=>{\n" +
-                        "                    if(itema==itemb){\n" +
-                        "                        window.lambo.performClick(index)\n" +
-                        "                    }\n" +
-                        "                })\n" +
-                        "            })})\n" +
-                        "        } else {\n" +
-                        "            checkElements()\n" +
-                        "        }\n" +
-                        "    },1000)\n" +
-                        "}\n" +
-                        "checkElements()"
-            }
-        }
-    }
-    fun manualEventListenerScript(): String {
-        when(name){
-            "laser247.com" -> {
-                return "function checkElements(){\n" +
-                        "    setTimeout(()=>{\n" +
-                        "        targetElements = document.querySelectorAll(\".odds_body button\")\n" +
-                        "        if (targetElements.length > 0){\n" +
-                        "            targetElements.forEach((item)=>{\n" +
-                        "                item.addEventListener(\"click\",(event)=>{\n" +
-                        "                var itemb = event.target\n" +
-                        "                targetElements.forEach((itema,index,arr)=>{\n" +
-                        "                    if(itema==itemb){\n" +
-                        "                        window.lambo.performClick(index,itemb.innerText)\n" +
-                        "                    }\n" +
-                        "                })\n" +
-                        "            })})\n" +
-                        "        } else {\n" +
-                        "            checkElements()\n" +
-                        "        }\n" +
-                        "    },1000)\n" +
-                        "}\n" +
-                        "checkElements()"
-            }
-            "lotus365.com" -> {
-                return "function clickEventListener(event){\n" +
-                        "    targetElements = document.querySelectorAll(\".SportEvent__market .odd-button\")\n" +
+                return "var targetElements = document.querySelectorAll(\".odds_body button\");\n" +
+                        "function clickEventListener(event){\n" +
+                        "    checkElements();\n" +
+                        "    var a = event.target;\n" +
                         "    var selected = null;\n" +
-                        "    targetElements.forEach((item,index,arr)=>{\n" +
-                        "        if(item===event.target){\n" +
-                        "            selected = index;\n" +
-                        "        }\n" +
-                        "    });\n" +
+                        "    if(a.className === \"odd-button__price\" || a.className === \"odd-button__volume\"){\n" +
+                        "        selected = a.parentElement.parentElement;\n" +
+                        "    } else {\n" +
+                        "        selected = event.target;\n" +
+                        "    }\n" +
+                        "    //console.log(selected.getAttribute(\"lambolistenning\"));\n" +
                         "    window.lambo.buttonCount(targetElements.length);\n" +
-                        "    window.lambo.performClick(selected);\n" +
+                        "    window.lambo.performClick(selected.getAttribute(\"lambolistenning\"));\n" +
                         "}\n" +
                         "function checkElements(){\n" +
-                        "    var targetElements = document.querySelectorAll(\".SportEvent__market .odd-button\")\n" +
-                        "    targetElements.forEach((item)=>{\n" +
-                        "        if(Boolean(item.hasAttribute(\"lambolistenning\"))){\n" +
-                        "            console.log(item.innerText);\n" +
+                        "    targetElements = document.querySelectorAll(\".odds_body button\")\n" +
+                        "    targetElements.forEach((item,index,arr)=>{\n" +
+                        "        if(item.hasAttribute(\"lambolistenning\")){\n" +
+                        "            //console.log(item.innerText);\n" +
                         "        } else {\n" +
                         "            item.addEventListener(\"click\",clickEventListener);\n" +
-                        "            item.setAttribute(\"lambolistenning\",\"true\");\n" +
                         "        }\n" +
+                        "        item.setAttribute(\"lambolistenning\",index);\n" +
                         "    });\n" +
                         "    window.lambo.buttonCount(targetElements.length);\n" +
                         "}\n" +
                         "checkElements();"
-            }
-            else -> {
-                return "function checkElements(){\n" +
-                        "    setTimeout(()=>{\n" +
-                        "        targetElements = document.querySelectorAll(\".odds_body button\")\n" +
-                        "        if (targetElements.length > 0){\n" +
-                        "            targetElements.forEach((item)=>{\n" +
-                        "                item.addEventListener(\"click\",(event)=>{\n" +
-                        "                var itemb = event.target\n" +
-                        "                targetElements.forEach((itema,index,arr)=>{\n" +
-                        "                    if(itema==itemb){\n" +
-                        "                        window.lambo.performClick(index)\n" +
-                        "                    }\n" +
-                        "                })\n" +
-                        "            })})\n" +
-                        "        } else {\n" +
-                        "            checkElements()\n" +
-                        "        }\n" +
-                        "    },1000)\n" +
-                        "}\n" +
-                        "checkElements()"
             }
         }
     }
